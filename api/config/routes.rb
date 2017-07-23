4d4eda4e9aa8679ff9783ab4f02166da3dd1aa96
Chris Zempel <zempel@efeqdev.com>
2017-07-23 15:39:49 -0500

    Adds fixtures and routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users do
+        resources :gifts
+      end
+
       post 'user_token' => 'user_token#create'
     end
   end
