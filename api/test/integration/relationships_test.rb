9bbb8f99f4cc5cfaed2bc8a9b8a35dec2d77fc50
Chris Zempel <zempel@efeqdev.com>
2017-08-31 08:03:05 -0500

    Adds in some tests to situate myself for when I dip in next time!

---
+++ api/test/integration/relationships_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class RelationshipsTest < ActionDispatch::IntegrationTest
+  test "make a friend request"
+  test "accept a friend request"
+  test "reject a friend request"
+  test "view pending requests"
+  test "view pending sent requests"
+  test "friends list"
+end
