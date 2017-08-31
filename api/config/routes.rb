f5927628017cf5b8e45eadb45f4821f4a0ec87ce
Chris Zempel <zempel@efeqdev.com>
2017-08-31 07:54:17 -0500

    Designs relationship endpoints

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users do
         resources :gifts
+        resources :relationships do
+          get :pending_requests,  to: "relationships/pending_requests#index", on: :collection
+          get :sent_requests,     to: "relationships/sent_requests#index",    on: :collection
+          put :accept,            to: "relationships/accepts#update",         on: :member
+          put :reject,            to: "relationships/rejects#update",         on: :member
+          put :block,             to: "relationships/blocks#update",          on: :member
+        end
+        get :friends, to: "relationships#index", on: :member
       end
 
       namespace :users do
